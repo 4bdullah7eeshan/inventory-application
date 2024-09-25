a82a747dd1afad0f1e8a33dd550f93d2a3302ae8
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-09-25 22:33:02 +0530

    feat: create getCategoryById controller
    This will handle viewing of a specific category in detail.

--- controllers/categoriesController.js
+++ controllers/categoriesController.js
@@ -16,9 +16,15 @@ const getNewCategory = asyncHandler(async (req, res) => {
     res.render("pages/newCategory", { title: "New Category" });
 });
 
+const getCategoryById = asyncHandler(async (req, res) => {
+    const categoryId = parseInt(req.params.id, 10); 
+    const category = await db.getCategoryById(categoryId);
+    res.render("pages/category", { title: category.name, category: category });
+});
 
 module.exports = {
     getAllCategories,
     createNewCategory,
     getNewCategory,
+    getCategoryById
 };
