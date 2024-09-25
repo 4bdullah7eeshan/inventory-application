730b8189e50501bd5d0baf44ca9f58458a269bac
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-09-25 22:10:45 +0530

    feat: create getAllCategories controller
    This will handle rendering of all categories in the respective views.

--- controllers/categoriesController.js
+++ controllers/categoriesController.js
@@ -1,6 +1,11 @@
 const db = require("../db/queries");
 const asyncHandler = require("express-async-handler");
 
+const getAllCategories = asyncHandler(async (req, res) => {
+    const categories = await db.getAllCategories();
+    res.render("pages/categories", { title: "All Categories", categories: categories });
+});
+
 module.exports = {
-    
+    getAllCategories,
 };
