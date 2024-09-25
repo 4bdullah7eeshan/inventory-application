addd1392ec657435b5e1d4a0805d8fcc72c74313
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-09-25 22:13:52 +0530

    feat: setup 'createNewCategory' controller
    This will handle adding a new category to the db

--- controllers/categoriesController.js
+++ controllers/categoriesController.js
@@ -6,6 +6,13 @@ const getAllCategories = asyncHandler(async (req, res) => {
     res.render("pages/categories", { title: "All Categories", categories: categories });
 });
 
+const createNewCategory = asyncHandler(async (req, res) => {
+    const { name, description, image } = req.body;
+    await db.insertNewCategory({ name: name, description: description, image: image });
+    res.redirect("/categories");
+});
+
 module.exports = {
     getAllCategories,
+    createNewCategory,
 };
