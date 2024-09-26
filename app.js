58061c848fa3ab3104114d991b872733f03b6aa7
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-09-27 01:27:20 +0530

    feat: add method-override middleware

--- app.js
+++ app.js
@@ -2,19 +2,27 @@ const express = require('express');
 const path = require("node:path");
 const indexRouter = require("./routes/indexRouter");
 const categoriesRouter = require("./routes/categoriesRouter");
+const methodOverride = require("method-override");
 
 const app = express();
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+
+app.use(methodOverride('_method'));
+
 const assetsPath = path.join(__dirname, "public");
 app.use(express.static(assetsPath));
+
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use("/", indexRouter);
 app.use("/categories", categoriesRouter);
 app.use("categories/new", categoriesRouter);
+
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Mini Message Board app running on port ${PORT}!`));
