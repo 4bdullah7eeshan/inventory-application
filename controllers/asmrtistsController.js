dfd210228b8748808ff6288bb9c325657bceff17
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-09-27 03:10:12 +0530

    feat: create a controller to get an asmrtist

--- controllers/asmrtistsController.js
+++ controllers/asmrtistsController.js
@@ -17,10 +17,17 @@ const createNewASMRtist = asyncHandler(async (req, res) => {
     res.redirect("/asmrtists");
 });
 
+const getASMRtistById = asyncHandler(async (req, res) => {
+    const asmrtistId = parseInt(req.params.id, 10); 
+    const asmrtist = await db.getAsmrtistById(asmrtistId);
+    res.render("pages/asmrtist", { title: asmrtist.name, asmrtist: asmrtist });
+});
+
 
 module.exports = {
     getAllAsmrtists,
     getNewAsmrtist,
     createNewASMRtist,
+    getASMRtistById,
     
 };
